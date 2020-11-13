eb9fc36e4225af71a67fda36255f487de65662a8
Joe Buono <joebuono724@gmail.com>
2020-11-12 22:40:12 -0500

    calculating global rating and star ratings

--- client/src/components/Ratings.jsx
+++ client/src/components/Ratings.jsx
@@ -1,7 +1,29 @@
 import React from 'react';
+import RatingBar from './RatingBar.jsx';
 
-const Ratings = () => (
-  <div>Ratings</div>
-);
+const Ratings = ({ratings}) => {
+  const totals = {};
+  let sumTotal = 0;
+  const len = ratings.length;
+
+  for (let rating of ratings) {
+    sumTotal += rating;
+    totals[rating] = ++totals[rating] || 1;
+  }
+
+  for (let rating in totals) {
+    totals[rating] = Math.round((totals[rating] / len) * 100);
+  }
+
+  const globalAverage = (sumTotal / len).toFixed(1);
+
+  return (
+    <div>
+      <div>Customer reviews</div>
+        <div>Stars... {globalAverage} out of 5</div>
+        <div>{len} global rating{len > 1 && 's'}</div>
+    </div>
+  );
+};
 
 export default Ratings;
