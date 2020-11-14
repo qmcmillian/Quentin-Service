cca438ea33cb773549067f168ae19c67b40cdc92
Joe Buono <joebuono724@gmail.com>
2020-11-13 21:12:29 -0500

    implemented dynamically filled star component, working on half star

--- client/src/components/Stars.jsx
+++ client/src/components/Stars.jsx
@@ -1,18 +1,20 @@
 import React from 'react';
 import styled from 'styled-components';
+import { FaStar } from 'react-icons/fa';
 
 // Star unicode:
 // \u2605
 
-const starStyles = {
-  color: '#FFA41C',
-  fontSize: '1.5em'
-};
+// const starStyles = {
+//   color: '#FFA41C'
+// };
 
 const Stars = ({rating}) => {
   return (
-    <div style={starStyles}>
-      {'\u2605\u2605\u2605\u2605\u2605'}
+    <div>
+      {[...Array(5)].map((star, index) => {
+        return <FaStar size={20} color={index + 1 <= rating ? '#FFA41C' : '#f2f2f2'}/>;
+      })}
     </div>
   )
 };
