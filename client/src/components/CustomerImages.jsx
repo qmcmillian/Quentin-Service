42be74354a9a04d2d46483eb31788e7bb2362c94
Joe Buono <joebuono724@gmail.com>
2020-11-17 00:23:12 -0500

    implemented modal for customer images

--- client/src/components/CustomerImages.jsx
+++ client/src/components/CustomerImages.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
+import Modal from './Modal.jsx';
 
 const Headline = styled.h1`
   font-family: 'PT Sans';
@@ -20,21 +21,36 @@ const SeeAllImages = styled.p`
   font-family: 'PT Sans';
   cursor: ${props => props.hover ? 'pointer' : 'none'};
   text-decoration: ${props => props.hover ? 'underline' : 'none'};
+  margin-top: 5px;
   margin-bottom: 0;
+  font-size: .9em;
+`;
+
+const NoImages = styled.h1`
+  font-family: 'PT Sans';
+  font-size: 1em;
+  font-weight: 400;
+  margin-top: 0;
+  margin-bottom: 10px;
 `;
 
 const CustomerImages = ({imageUrls}) => {
   const [onHover, setOnHover] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
     <div>
       <Headline>Customer images</Headline>
+      {!imageUrls.length ? <NoImages>No customer images yet.</NoImages> :
+      <div>
       {imageUrls.map((image, index) => {
         if (image && index < 4) {
           return <img src={image} style={imgStyle}/>
         }
       })}
-      <SeeAllImages hover={onHover} onMouseEnter={() => setOnHover(true)} onMouseLeave={() => setOnHover(false)}>See all customer images</SeeAllImages>
+      <SeeAllImages hover={onHover} onMouseEnter={() => setOnHover(true)} onMouseLeave={() => setOnHover(false)} onClick={() => setIsOpen(true)}>See all customer images</SeeAllImages>
+      <Modal imageUrls={imageUrls} open={isOpen} onClose={() => setIsOpen(false)}></Modal>
+      </div>}
     </div>
   )
 };
