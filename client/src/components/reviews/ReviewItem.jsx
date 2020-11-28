62623672985f8f305f0c2b258a1397c17f210880
Joe Buono <joebuono724@gmail.com>
2020-11-27 21:34:22 -0500

    cruising through refactor

--- client/src/components/reviews/ReviewItem.jsx
+++ client/src/components/reviews/ReviewItem.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import Stars from '../styles/Stars.jsx';
-import { Box, ReviewItemHeadline, ReviewText, ReviewGrayText, Comment, Report, ThinLine, Verified, HelpfulBtn } from '../styles/Styles.jsx';
+import { ReviewGrayText, Comment, Report, ThinLine, Verified, HelpfulBtn } from '../styles/Styles.jsx';
 import s from '../styles/Reviews.css';
 
 const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
-const ReviewItem = ({review, keyword}) => {
-  const {avatar, country, full_text, headline, overall_rating, review_date, user_name, verified_purchase, helpful} = review;
+const ReviewItem = ({ review, keyword }) => {
+  const { avatar, country, full_text, headline, overall_rating, review_date, user_name, verified_purchase, helpful } = review;
 
   let [year, month, day] = review_date.split('T')[0].split('-');
 
@@ -18,35 +18,35 @@ const ReviewItem = ({review, keyword}) => {
     return (<span>
       { text.split(keyword)
         .reduce((acc, current) => {
-          return acc.concat(<span className={s.highlight} key={keyword + current}>{ keyword }</span>, current);
+          return acc.concat(<span className={s.highlight} key={keyword + current}>{keyword}</span>, current);
         }, [])
       }
     </span>);
   };
 
   return (
-  <div className={s.reviewItemContainer}>
-    <div className={s.avatar}>
-      <img src={avatar} style={{borderRadius: '100%', width: '30px', height: '30px', objectFit: 'cover'}}/>
-      <div style={{marginLeft: '10px', fontFamily: 'PT Sans', fontSize: '.85em'}}>{user_name}</div>
+    <div className={s.reviewItemContainer}>
+      <div className={s.avatar}>
+        <img src={avatar} style={{ borderRadius: '100%', width: '30px', height: '30px', objectFit: 'cover' }} />
+        <div style={{ marginLeft: '10px', fontFamily: 'PT Sans', fontSize: '.85em' }}>{user_name}</div>
+      </div>
+      <div className={s.box}>
+        <Stars rating={overall_rating} height={'15px'} /><div className={s.reviewItemHeadline}>{headline}</div>
+      </div>
+      <ReviewGrayText>
+        Reviewed in {country} on {`${months[month - 1]} ${day}, ${year}`}
+      </ReviewGrayText>
+      {verified_purchase === 1 && <Verified>Verified Purchase</Verified>}
+      <p className={s.reviewText}>{highlightText(full_text, keyword)}</p>
+      <ReviewGrayText>
+        {helpful !== 1 ? `${helpful} people found this helpful` : 'One person found this helpful'}
+      </ReviewGrayText>
+      <div style={{ display: 'inline-flex', alignItems: 'center', width: '300px', height: '50px' }}>
+        <HelpfulBtn>Helpful</HelpfulBtn>
+        <ThinLine><Comment>Comment</Comment></ThinLine>
+        <ThinLine><Report>Report abuse</Report></ThinLine>
+      </div>
     </div>
-    <Box>
-      <Stars rating={overall_rating} height={'15px'}/><ReviewItemHeadline>{headline}</ReviewItemHeadline>
-    </Box>
-    <ReviewGrayText>
-      Reviewed in {country} on {`${months[month - 1]} ${day}, ${year}`}
-    </ReviewGrayText>
-    {verified_purchase === 1 && <Verified>Verified Purchase</Verified>}
-    <ReviewText>{highlightText(full_text, keyword)}</ReviewText>
-    <ReviewGrayText>
-      {helpful !== 1 ? `${helpful} people found this helpful` : 'One person found this helpful'}
-    </ReviewGrayText>
-    <div style={{display: 'inline-flex', alignItems: 'center', width: '300px', height: '50px'}}>
-      <HelpfulBtn>Helpful</HelpfulBtn>
-      <ThinLine><Comment>Comment</Comment></ThinLine>
-      <ThinLine><Report>Report abuse</Report></ThinLine>
-    </div>
-  </div>
   );
 };
 
